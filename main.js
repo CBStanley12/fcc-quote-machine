70caa016738dae780f3f19951757a7b79941abb6
CBStanley12 <cbstanley12@gmail.com>
2019-01-20 16:14:43 -0700

    Display random image for new quotes

--- main.js
+++ main.js
@@ -13,6 +13,8 @@ $(document).ready(function() {
 		cache: false
 	});
 
+	$('body').css({'background-image': "url('https://source.unsplash.com/random/1600x900')"});
+
   	$('#new-quote').on('click', function() {
 	    $.ajax({
 	      url: "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=''$_jsonp=?",
@@ -22,5 +24,7 @@ $(document).ready(function() {
 	        $('#text').html(post.content);
 	      }
 	    });
+	    var sourceImage = "url('https://source.unsplash.com/featured/1600x900?sig=" + Math.random() + "')";
+    	$('body').css({'background-image': sourceImage});
 	});
 });
