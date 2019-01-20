231c646276b73e8176ad8de6cb2d05f4cb767529
CBStanley12 <cbstanley12@gmail.com>
2019-01-20 16:16:38 -0700

    Added tweet quote functionality

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ $(document).ready(function() {
 		cache: false
 	});
 
+	$('#tweet-quote').attr('href', "https://twitter.com/intent/tweet?text=" + $('#text').text().replace(/\s/g,"%20") + " - " + $('#author').text().replace(/\s/g,"%20"));
 	$('body').css({'background-image': "url('https://source.unsplash.com/random/1600x900')"});
 
   	$('#new-quote').on('click', function() {
@@ -27,4 +28,9 @@ $(document).ready(function() {
 	    var sourceImage = "url('https://source.unsplash.com/featured/1600x900?sig=" + Math.random() + "')";
     	$('body').css({'background-image': sourceImage});
 	});
+
+	$('#tweet-quote').on('click', function() {
+	    $('#tweet-quote').attr('href', 'https://twitter.com/intent/tweet?text="' + $('#text').text().replace(/\s/g,"%20") + '" - ' + $('#author').text().replace(/\s/g,"%20"));
+	  });
+	
 });
