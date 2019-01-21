e687c0137a21ea75b629a14da2a2efb03d746ebf
CBStanley12 <cbstanley12@gmail.com>
2019-01-20 20:25:35 -0700

    Added transitions for main elements

--- main.js
+++ main.js
@@ -14,9 +14,12 @@ $(document).ready(function() {
 	});
 
 	$('#tweet-quote').attr('href', "https://twitter.com/intent/tweet?text=" + $('#text').text().replace(/\s/g,"%20") + " - " + $('#author').text().replace(/\s/g,"%20"));
-	$('body').css({'background-image': "url('https://source.unsplash.com/random/1600x900')"});
+	$('.bgImg').attr('src', "https://source.unsplash.com/random/1600x900");
 
   	$('#new-quote').on('click', function() {
+  		$('.blockquote').fadeOut('slow');
+  		$('.bgImg').fadeOut('slow');
+
 	    $.ajax({
 	      url: "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=''$_jsonp=?",
 	      success: function(data) {
@@ -25,12 +28,16 @@ $(document).ready(function() {
 	        $('#text').html(post.content);
 	      }
 	    });
-	    var sourceImage = "url('https://source.unsplash.com/featured/1600x900?sig=" + Math.random() + "')";
-    	$('body').css({'background-image': sourceImage});
+
+	    var sourceImage = "https://source.unsplash.com/featured/1600x900?sig=" + Math.random();
+    	$('.bgImg').attr('src', sourceImage);
+
+    	$('.blockquote').fadeIn('slow');
+    	$('.bgImg').fadeIn('slow');
 	});
 
 	$('#tweet-quote').on('click', function() {
 	    $('#tweet-quote').attr('href', 'https://twitter.com/intent/tweet?text="' + $('#text').text().replace(/\s/g,"%20") + '" - ' + $('#author').text().replace(/\s/g,"%20"));
-	  });
+	});
 	
 });
